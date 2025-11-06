03799c9e02263b465c380232410c4a0aa9d802c0
SakshiKoli-CS <sakshi.koli@contentstack.com>
2025-11-06 19:53:00 +0530

    Fix: Uncomment edge function code

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,22 +1,22 @@
-// /**
-//  * Contentstack Launch Edge Function
-//  * Redirect from /about to /ssg
-//  */
+/**
+ * Contentstack Launch Edge Function
+ * Redirect from /about to /ssg
+ */
 
-// export default async function handler(request, context) {
-//   const parsedUrl = new URL(request.url);
-//   const pathname = parsedUrl.pathname;
+export default async function handler(request, context) {
+  const parsedUrl = new URL(request.url);
+  const pathname = parsedUrl.pathname;
 
-//   console.log(`[Edge Function] ${request.method} ${pathname}`);
+  console.log(`[Edge Function] ${request.method} ${pathname}`);
 
-//   // Redirect /about to /ssg (301 Permanent Redirect)
-//   if (pathname === '/about') {
-//     const newUrl = new URL(request.url);
-//     newUrl.pathname = '/ssg';
-//     return Response.redirect(newUrl.toString(), 301);
-//   }
+  // Redirect /about to /ssg (301 Permanent Redirect)
+  if (pathname === '/about') {
+    const newUrl = new URL(request.url);
+    newUrl.pathname = '/ssg';
+    return Response.redirect(newUrl.toString(), 301);
+  }
 
-//   // Forward all other requests to the origin server
-//   return fetch(request);
-// }
+  // Forward all other requests to the origin server
+  return fetch(request);
+}
 
