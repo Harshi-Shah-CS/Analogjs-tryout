b03466bc27bb782a57c9359f91c190789ef04a87
SakshiKoli-CS <sakshi.koli@contentstack.com>
2025-11-06 19:20:04 +0530

    Add Contentstack Launch edge function for /about to /ssg redirect

---
+++ functions/[proxy].edge.js
@@ -0,0 +1,24 @@
+/**
+ * Contentstack Launch Edge Function
+ * Based on: https://www.contentstack.com/docs/developers/launch/edge-functions
+ * 
+ * Redirect from /about to /ssg
+ */
+
+export default async function handler(request, context) {
+  const parsedUrl = new URL(request.url);
+  const pathname = parsedUrl.pathname;
+
+  console.log(`[Edge Function] ${request.method} ${pathname}`);
+
+  // Redirect /about to /ssg (301 Permanent Redirect)
+  if (pathname === '/about') {
+    const newUrl = new URL(request.url);
+    newUrl.pathname = '/ssg';
+    return Response.redirect(newUrl.toString(), 301);
+  }
+
+  // Forward all other requests to the origin server
+  return fetch(request);
+}
+
