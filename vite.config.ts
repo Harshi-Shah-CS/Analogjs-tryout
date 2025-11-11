167d82c6bfa393dc359772e159bb870c6709528b
SakshiKoli-CS <sakshi.koli@contentstack.com>
2025-11-11 12:16:18 +0530

    add nitro for ssr

--- vite.config.ts
+++ vite.config.ts
@@ -24,6 +24,11 @@ export default defineConfig(({ mode }) => ({
           '/test',
         ],
       },
+      nitro: {
+        routeRules: {
+          '/products': { proxy: '/test' },
+        },
+      },
     }),
     tailwindcss()
   ],
